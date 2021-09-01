10bb839d4a0b0781ad8b0b98ff52b337c37e80ae
Galih Andyan Anindita <galih@Galihs-MacBook-Pro.local>
2021-09-01 19:27:24 +0700

    new commit
    add some line in second_main
    
    1
    
    2
    
    merge 3 into single commit
    
    sesuatu
    
    apapa

--- first_main.js
+++ first_main.js
@@ -1,1 +1,3 @@
 console.log("first on main")
+
+console.log("another changes")
---
+++ second_main.js
@@ -0,0 +1,15 @@
+console.log("second on main")
+
+console.log("add another")
+
+console.log("asdas")
+
+console.log("object")
+
+console.log('gi')
+
+console.log("object")
+
+console.log("object")
+
+console.log("adasdsa")
